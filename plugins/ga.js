5e6840d756029f3e14f67deb8c86177447b55ee7
Stefano Pietroiusti <11223707+pietroiusti@users.noreply.github.com>
2019-10-12 19:56:31 +1300

    ga update to remove client error

--- plugins/ga.js
+++ plugins/ga.js
@@ -25,6 +25,6 @@ export default ({ app }) => {
     */
     ga('set', 'page', to.fullPath)
     ga('send', 'pageview')
-    ga('send', 'event', 'page', to.fullPath, JSON.stringify(to))
+    // ga('send', 'event', 'page', to.fullPath, JSON.stringify(to)) //causes client side error
   })
 }
